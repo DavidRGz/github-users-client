fb371410005ce093d4dd9d54360c8d291e855724
David Rodriguez Gonzalez <davidrguez_29@hotmail.com>
2022-12-01 08:14:06 -0600

    Se agregan rutas en App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
+import Users from "./pages/Users";
+import Repos from "./pages/Repos";
+import Commits from "./pages/Commits";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/:username" element={<Repos />} />
+        <Route path="/:owner/:repo" element={<Commits />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
