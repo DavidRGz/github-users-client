36898dda2b95351ecb333267dc9d7175b4821857
David Rodriguez Gonzalez <davidrguez_29@hotmail.com>
2022-12-01 17:53:26 -0600

    Se actualiza pages/Users con el componente Pagination

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -3,26 +3,35 @@ import { Container } from "reactstrap";
 
 import List from "../components/List";
 import UserItem from "../components/UserItem";
+import Pagination from "../components/Pagination";
 import { listUsers } from "../api/users";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [cursor, setCursor] = useState(0);
 
   useEffect(() => {
-    listUsers({ since: 0, per_page: 10 })
+    listUsers({ since: cursor, per_page: 10 })
       .then((data) => {
         setUsers(data);
       })
       .catch((error) => {
         alert(error.response?.data?.message || error.message);
       });
-  }, []);
+  }, [cursor]);
 
   return (
     <Container>
       <h2>Users</h2>
       <hr className="my-2" />
       <List items={users} renderItem={UserItem} />
+      <Pagination
+        cursor={cursor}
+        nextCursor={() => users[users.length - 1].id}
+        onChange={setCursor}
+        isFirst={cursor === 0}
+        isLast={users.length < 10}
+      />
     </Container>
   );
 };
