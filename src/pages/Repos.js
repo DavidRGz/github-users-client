40a01742ef15b3ad0a832cdd9ed2587a1e6f9a30
David Rodriguez Gonzalez <davidrguez_29@hotmail.com>
2022-12-01 18:02:07 -0600

    Se actualiza pages/Repos con el componente Pagination

--- src/pages/Repos.js
+++ src/pages/Repos.js
@@ -4,26 +4,36 @@ import { Container } from "reactstrap";
 
 import List from "../components/List";
 import RepoItem from "../components/RepoItem";
+import Pagination from "../components/Pagination";
 import { listRepos } from "../api/repos";
 
 const Repos = () => {
   const { username } = useParams();
   const [repos, setRepos] = useState([]);
+  const [cursor, setCursor] = useState(1);
 
   useEffect(() => {
-    listRepos({ username })
+    listRepos({ username, page: cursor, per_page: 10 })
       .then((data) => {
         setRepos(data);
       })
       .catch((error) => {
         alert(error.response?.data?.message || error.message);
       });
-  }, [username]);
+  }, [username, cursor]);
 
   return (
     <Container>
       <h2>Repos</h2>
+      <hr className="my-2" />
       <List items={repos} renderItem={RepoItem} />
+      <Pagination
+        cursor={cursor}
+        nextCursor={() => cursor + 1}
+        onChange={setCursor}
+        isFirst={cursor === 1}
+        isLast={repos.length < 10}
+      />
     </Container>
   );
 };
