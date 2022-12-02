57d516aab08fbafe39765442f0b404eb20d3a507
David Rodriguez Gonzalez <davidrguez_29@hotmail.com>
2022-12-01 18:04:43 -0600

    Se actualiza pages/Commits con el componente Pagination

--- src/pages/Commits.js
+++ src/pages/Commits.js
@@ -4,17 +4,19 @@ import { useParams } from "react-router-dom";
 
 import List from "../components/List";
 import CommitItem from "../components/CommitItem";
+import Pagination from "../components/Pagination";
 import { listCommits } from "../api/commits";
 
 const Commits = () => {
   const { owner, repo } = useParams();
   const [commits, setCommits] = useState([]);
+  const [cursor, setCursor] = useState(1);
 
   useEffect(() => {
     listCommits({
       owner,
       repo,
-      page: 1,
+      page: cursor,
       per_page: 10,
     })
       .then((data) => {
@@ -23,13 +25,20 @@ const Commits = () => {
       .catch((error) => {
         alert(error.response?.data?.message || error.message);
       });
-  }, [owner, repo]);
+  }, [owner, repo, cursor]);
 
   return (
     <Container>
       <h2>Commits</h2>
       <hr className="my-2" />
       <List keyName="sha" items={commits} renderItem={CommitItem} />
+      <Pagination
+        cursor={cursor}
+        nextCursor={() => cursor + 1}
+        onChange={setCursor}
+        isFirst={cursor === 1}
+        isLast={commits.length < 10}
+      />
     </Container>
   );
 };
